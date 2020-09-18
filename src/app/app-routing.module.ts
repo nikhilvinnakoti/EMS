ceca251639714e46bb6ed43800dc655b54163c6d
Nikhil Vinnakoti <you@example.com>
2020-09-18 13:30:31 +0530

    second commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CheckinComponent } from './checkin/checkin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'dashboard',component: DashboardComponent},
+  {path:'checkin',component: CheckinComponent},
+ 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
