e186a310944a9c82ac2d521ebb85f6ee41873deb
Nikhil Vinnakoti <you@example.com>
2020-09-23 13:13:26 +0530

    Seventh Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,7 @@ import { AppComponent } from './app.component';
 import { AttendenceListComponent } from './attendence-list/attendence-list.component';
 import { BirthdayComponent } from './birthday/birthday.component';
 import { ChangePasswordComponent } from './change-password/change-password.component';
-import { PersonalDetailsBankComponent } from './personal-details-bank/personal-details-bank.component';
-import { PersonalDetailsEditComponent } from './personal-details-edit/personal-details-edit.component';
-import { PersonalDetailsQualificationComponent } from './personal-details-qualification/personal-details-qualification.component';
-import { TaskDetailsComponent } from './task-details/task-details.component';
-import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+
 
 @NgModule({
   declarations: [
@@ -18,11 +14,7 @@ import { PersonalDetailsComponent } from './personal-details/personal-details.co
     AttendenceListComponent,
     BirthdayComponent,
     ChangePasswordComponent,
-    PersonalDetailsComponent,
-    PersonalDetailsBankComponent,
-    PersonalDetailsEditComponent,
-    PersonalDetailsQualificationComponent,
-    TaskDetailsComponent
+    
   ],
   imports: [
     BrowserModule,
