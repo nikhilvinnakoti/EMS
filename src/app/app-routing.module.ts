ac6514f2f2a18f8732cf88dad13f4c5754857c90
Nikhil Vinnakoti <you@example.com>
2020-09-23 17:01:22 +0530

    Twelth commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,31 @@
-// import { NgModule } from '@angular/core';
-// import { Routes, RouterModule } from '@angular/router';
-// import { AddTasksToCheckoutComponent } from './add-tasks-to-checkout/add-tasks-to-checkout.component';
-// import { CheckinComponent } from './checkin/checkin.component';
-// import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
-// import { CheckoutComponent } from './checkout/checkout.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
-// import { LoginComponent } from './login/login.component';
-// import { PersonalDetailsBankComponent } from './personal-details-bank/personal-details-bank.component';
-// import { PersonalDetailsEditComponent } from './personal-details-edit/personal-details-edit.component';
-// import { PersonalDetailsQualificationComponent } from './personal-details-qualification/personal-details-qualification.component';
-// import { PersonalDetailsComponent } from './personal-details/personal-details.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddTasksToCheckoutComponent } from './add-tasks-to-checkout/add-tasks-to-checkout.component';
+import { CheckinComponent } from './checkin/checkin.component';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { PersonalDetailsBankComponent } from './personal-details-bank/personal-details-bank.component';
+import { PersonalDetailsEditComponent } from './personal-details-edit/personal-details-edit.component';
+import { PersonalDetailsQualificationComponent } from './personal-details-qualification/personal-details-qualification.component';
+import { PersonalDetailsComponent } from './personal-details/personal-details.component';
 
 
 
-// const routes: Routes = [
-//   { path: '', redirectTo: '/login', pathMatch: 'full' },
-//   { path: 'login', component: LoginComponent },
-//   {path: 'dashboard', component: DashboardComponent},
-//   { path: 'checkin', component: CheckinComponent },
-//   { path: 'checkout', component: CheckoutComponent },
-//   { path: 'add-tasks-to-checkout', component: AddTasksToCheckoutComponent },
-//   { path: 'checkout-success', component: CheckoutSuccessComponent }
-// ];
+const routes: Routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'checkin', component: CheckinComponent },
+    { path: 'checkout', component: CheckoutComponent },
+    { path: 'add-tasks-to-checkout', component: AddTasksToCheckoutComponent },
+    { path: 'checkout-success', component: CheckoutSuccessComponent }
+];
 
-// @NgModule({
-//   imports: [RouterModule.forRoot(routes)],
-//   exports: [RouterModule]
-// })
-// export class AppRoutingModule { }
-// export const routingComponents = [LoginComponent, DashboardComponent, CheckinComponent, CheckoutComponent, AddTasksToCheckoutComponent, CheckoutSuccessComponent,PersonalDetailsComponent,PersonalDetailsBankComponent,PersonalDetailsEditComponent,PersonalDetailsQualificationComponent]
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [LoginComponent, DashboardComponent, CheckinComponent, CheckoutComponent, AddTasksToCheckoutComponent, CheckoutSuccessComponent, PersonalDetailsComponent, PersonalDetailsBankComponent, PersonalDetailsEditComponent, PersonalDetailsQualificationComponent]
