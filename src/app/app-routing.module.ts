8a48ddae3bf7414c3b87e0d15862c33a6b0b8035
Nikhil Vinnakoti <you@example.com>
2020-09-23 15:51:44 +0530

    Nineth Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { AddTasksToCheckoutComponent } from './add-tasks-to-checkout/add-tasks-t
 import { CheckinComponent } from './checkin/checkin.component';
 import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 import { CheckoutComponent } from './checkout/checkout.component';
-import { ChildComponent } from './child/child.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
 
@@ -13,23 +12,11 @@ import { LoginComponent } from './login/login.component';
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  {
-    path: 'dashboard', component: DashboardComponent, children: [
-      { path: 'child', component: ChildComponent }
-    ]
-  },
-  { path: 'checkin', component: CheckinComponent,children:[
-    { path: 'child', component: ChildComponent }
-  ] },
-  { path: 'checkout', component: CheckoutComponent, children:[
-    { path: 'child', component: ChildComponent }
-  ] },
-  { path: 'add-tasks-to-checkout', component: AddTasksToCheckoutComponent,children:[
-    { path: 'child', component: ChildComponent }
-  ] },
-  { path: 'checkout-success', component: CheckoutSuccessComponent,children:[
-    { path: 'child', component: ChildComponent }
-  ] }
+  {path: 'dashboard', component: DashboardComponent},
+  { path: 'checkin', component: CheckinComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  { path: 'add-tasks-to-checkout', component: AddTasksToCheckoutComponent },
+  { path: 'checkout-success', component: CheckoutSuccessComponent }
 ];
 
 @NgModule({
@@ -37,4 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [LoginComponent, DashboardComponent, CheckinComponent, CheckoutComponent, AddTasksToCheckoutComponent, CheckoutSuccessComponent, ChildComponent]
+export const routingComponents = [LoginComponent, DashboardComponent, CheckinComponent, CheckoutComponent, AddTasksToCheckoutComponent, CheckoutSuccessComponent]
